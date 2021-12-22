f833c5898ee630fa04dc10e1e434accd7c79ce24
应卓 <yingzhor@gmail.com>
2021-12-22 09:36:29 +0800

    2021-12-22 09:36:27

--- spring-turbo-module-feign/src/main/java/spring/turbo/module/feign/FeignClientFactoryBean.java
+++ spring-turbo-module-feign/src/main/java/spring/turbo/module/feign/FeignClientFactoryBean.java
@@ -9,6 +9,7 @@
 package spring.turbo.module.feign;
 
 import feign.Request;
+import feign.RequestInterceptor;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.slf4j.Slf4jLogger;
@@ -18,12 +19,15 @@ import org.springframework.beans.factory.SmartFactoryBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
+import org.springframework.core.OrderComparator;
 import org.springframework.core.env.Environment;
 import spring.turbo.core.AnnotationUtils;
 import spring.turbo.module.feign.annotation.*;
 import spring.turbo.util.Asserts;
 import spring.turbo.util.InstanceCache;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static feign.Feign.Builder;
@@ -57,17 +61,18 @@ class FeignClientFactoryBean implements SmartFactoryBean, InitializingBean, Appl
         capabilities(builder, clientType);
         options(builder, clientType);
         doNotCloseAfterDecode(builder, clientType);
+        requestInterceptors(builder, clientType);
         customizer(builder, clientType);
         return builder.target(clientType, url);
     }
 
-    private void decoded404(final Builder builder, final Class<?> clientType) {
+    protected void decoded404(final Builder builder, final Class<?> clientType) {
         if (AnnotationUtils.findAnnotation(clientType, Decoded404.class) != null) {
             builder.decode404();
         }
     }
 
-    private void slf4j(final Builder builder, final Class<?> clientType) {
+    protected void slf4j(final Builder builder, final Class<?> clientType) {
         Optional.ofNullable(AnnotationUtils.findAnnotation(clientType, Slf4j.class))
                 .ifPresent(a -> {
                     builder.logger(new Slf4jLogger(clientType));
@@ -75,7 +80,7 @@ class FeignClientFactoryBean implements SmartFactoryBean, InitializingBean, Appl
                 });
     }
 
-    private void encoderAndDecoder(final Builder builder, final Class<?> clientType) {
+    protected void encoderAndDecoder(final Builder builder, final Class<?> clientType) {
         EncoderAndDecoder annotation = AnnotationUtils.findAnnotation(clientType, EncoderAndDecoder.class);
         if (annotation != null) {
             Encoder encoder = instanceCache.findOrCreate(annotation.encoderType());
@@ -85,35 +90,35 @@ class FeignClientFactoryBean implements SmartFactoryBean, InitializingBean, Appl
         }
     }
 
-    private void errorDecoder(final Builder builder, final Class<?> clientType) {
+    protected void errorDecoder(final Builder builder, final Class<?> clientType) {
         ErrorDecoder annotation = AnnotationUtils.findAnnotation(clientType, ErrorDecoder.class);
         if (annotation != null) {
             builder.errorDecoder(instanceCache.findOrCreate(annotation.type()));
         }
     }
 
-    private void client(final Builder builder, final Class<?> clientType) {
+    protected void client(final Builder builder, final Class<?> clientType) {
         Client annotation = AnnotationUtils.findAnnotation(clientType, Client.class);
         if (annotation != null) {
             builder.client(instanceCache.findOrCreate(annotation.type()));
         }
     }
 
-    private void queryMapEncoder(final Builder builder, final Class<?> clientType) {
+    protected void queryMapEncoder(final Builder builder, final Class<?> clientType) {
         QueryMapEncoder annotation = AnnotationUtils.findAnnotation(clientType, QueryMapEncoder.class);
         if (annotation != null) {
             builder.queryMapEncoder(instanceCache.findOrCreate(annotation.type()));
         }
     }
 
-    private void contract(final Builder builder, final Class<?> clientType) {
+    protected void contract(final Builder builder, final Class<?> clientType) {
         Contract annotation = AnnotationUtils.findAnnotation(clientType, Contract.class);
         if (annotation != null) {
             builder.contract(instanceCache.findOrCreate(annotation.type()));
         }
     }
 
-    private void capabilities(final Builder builder, final Class<?> clientType) {
+    protected void capabilities(final Builder builder, final Class<?> clientType) {
         Capabilities annotation = AnnotationUtils.findAnnotation(clientType, Capabilities.class);
         if (annotation != null) {
             for (Class<? extends feign.Capability> type : annotation.types()) {
@@ -124,7 +129,7 @@ class FeignClientFactoryBean implements SmartFactoryBean, InitializingBean, Appl
         }
     }
 
-    private void options(final Builder builder, final Class<?> clientType) {
+    protected void options(final Builder builder, final Class<?> clientType) {
         Options annotation = AnnotationUtils.findAnnotation(clientType, Options.class);
         if (annotation != null) {
             Request.Options op = new Request.Options(
@@ -138,14 +143,28 @@ class FeignClientFactoryBean implements SmartFactoryBean, InitializingBean, Appl
         }
     }
 
-    private void doNotCloseAfterDecode(final Builder builder, final Class<?> clientType) {
+    protected void doNotCloseAfterDecode(final Builder builder, final Class<?> clientType) {
         DoNotCloseAfterDecode annotation = AnnotationUtils.findAnnotation(clientType, DoNotCloseAfterDecode.class);
         if (annotation != null) {
             builder.doNotCloseAfterDecode();
         }
     }
 
-    private void customizer(final Builder builder, final Class<?> clientType) {
+    protected void requestInterceptors(final Builder builder, final Class<?> clientType) {
+        RequestInterceptors annotation = AnnotationUtils.findAnnotation(clientType, RequestInterceptors.class);
+        if (annotation != null) {
+            List<RequestInterceptor> interceptors = new ArrayList<>();
+            for (Class<? extends feign.RequestInterceptor> type : annotation.types()) {
+                if (type != null) {
+                    interceptors.add(instanceCache.findOrCreate(type));
+                }
+            }
+            OrderComparator.sort(interceptors);
+            builder.requestInterceptors(interceptors);
+        }
+    }
+
+    protected void customizer(final Builder builder, final Class<?> clientType) {
         Customizer annotation = AnnotationUtils.findAnnotation(clientType, Customizer.class);
         if (annotation != null && annotation.value() != null) {
             BuilderCustomizer bean = instanceCache.findOrCreate(annotation.value());
