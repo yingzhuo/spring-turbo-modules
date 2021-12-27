d024dcd48443897cc0e47e30906d035663279fad
应卓 <yingzhor@gmail.com>
2021-12-27 10:02:28 +0800

    2021-12-27 10:02:27

--- spring-turbo-module-qrcode/src/main/java/spring/turbo/module/qrcode/QRCodeGenerator.java
+++ spring-turbo-module-qrcode/src/main/java/spring/turbo/module/qrcode/QRCodeGenerator.java
@@ -14,6 +14,7 @@ import java.awt.image.BufferedImage;
 
 /**
  * @author 应卓
+ * @see spring.turbo.module.security.webmvc.entity.ImageResponseEntity
  * @since 1.0.0
  */
 public interface QRCodeGenerator {
