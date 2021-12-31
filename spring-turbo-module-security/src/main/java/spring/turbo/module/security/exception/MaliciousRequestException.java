3a66262dcf2ad7580c05c01ca2cbd9cafcab1b5f
应卓 <yingzhor@gmail.com>
2021-12-31 11:25:44 +0800

    2021-12-31 11:25:42

--- spring-turbo-module-security/src/main/java/spring/turbo/module/security/exception/MaliciousRequestException.java
+++ spring-turbo-module-security/src/main/java/spring/turbo/module/security/exception/MaliciousRequestException.java
@@ -11,19 +11,16 @@ package spring.turbo.module.security.exception;
 import org.springframework.security.core.AuthenticationException;
 
 /**
- * 恶意的http请求
+ * 恶意的请求
  *
  * @author 应卓
+ * @see spring.turbo.module.security.event.MaliciousRequestFailureEvent
  * @since 1.0.4
  */
 public class MaliciousRequestException extends AuthenticationException {
 
     public MaliciousRequestException(String msg) {
         super(msg);
-    }
-
-    public MaliciousRequestException(String msg, Throwable cause) {
-        super(msg, cause);
     }
 
 }
