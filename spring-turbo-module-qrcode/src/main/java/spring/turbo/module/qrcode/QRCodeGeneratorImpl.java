57b268757ba223945c003e216f706ba7ac4bcc5d
应卓 <yingzhor@gmail.com>
2022-01-09 09:13:35 +0800

    2022-01-09 09:13:33

--- spring-turbo-module-qrcode/src/main/java/spring/turbo/module/qrcode/QRCodeGeneratorImpl.java
+++ spring-turbo-module-qrcode/src/main/java/spring/turbo/module/qrcode/QRCodeGeneratorImpl.java
@@ -14,6 +14,7 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import spring.turbo.util.CharsetPool;
 
 import java.awt.*;
 import java.awt.geom.RoundRectangle2D;
@@ -23,15 +24,13 @@ import java.io.UncheckedIOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spring.turbo.util.StringPool.UTF_8;
-
 /**
  * @author 应卓
  * @since 1.0.0
  */
 public class QRCodeGeneratorImpl implements QRCodeGenerator {
 
-    private static final String CHARSET = UTF_8;
+    private static final String CHARSET = CharsetPool.UTF_8_VALUE;
 
     private ErrorCorrectionLevel defaultErrorCorrectionLevel = ErrorCorrectionLevel.H;
     private int defaultSize = 200;
