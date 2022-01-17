6c01eab4d2771a1a2e2a06db73ffd8776cea58db
应卓 <yingzhor@gmail.com>
2022-01-17 08:05:54 +0800

    2022-01-17 08:05:52

---
+++ spring-turbo-module-csv/src/main/java/spring/turbo/module/csv/vistor/NullBatchVisitor.java
@@ -0,0 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.csv.vistor;
+
+/**
+ * @param <T> ValueObject泛型
+ * @author 应卓
+ * @since 1.0.9
+ */
+@SuppressWarnings("unchecked")
+public final class NullBatchVisitor<T> implements BatchVisitor<T> {
+
+    private static final NullBatchVisitor INSTANCE = new NullBatchVisitor();
+
+    private NullBatchVisitor() {
+        super();
+    }
+
+    public static <T> NullBatchVisitor<T> getInstance() {
+        return INSTANCE;
+    }
+
+}
