7d14166254faa3790d4b8abcdba44c4ad3e549eb
应卓 <yingzhor@gmail.com>
2022-12-03 19:35:46 +0800

    2022-12-03 19:35:45

---
+++ spring-turbo-module-security/src/main/java/spring/turbo/module/security/util/PasswordEncoderUtils.java
@@ -0,0 +1,39 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.security.util;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import spring.turbo.core.SpringUtils;
+import spring.turbo.util.Asserts;
+
+/**
+ * @author 应卓
+ * @since 2.0.1
+ */
+public final class PasswordEncoderUtils {
+
+    /**
+     * 私有构造方法
+     */
+    private PasswordEncoderUtils() {
+        super();
+    }
+
+    public String encode(CharSequence rawPwd) {
+        Asserts.notNull(rawPwd);
+        return SpringUtils.getRequiredBean(PasswordEncoder.class).encode(rawPwd);
+    }
+
+    public boolean matches(CharSequence rawPwd, String encodedPassword) {
+        Asserts.notNull(rawPwd);
+        Asserts.notNull(encodedPassword);
+        return SpringUtils.getRequiredBean(PasswordEncoder.class).matches(rawPwd, encodedPassword);
+    }
+
+}
