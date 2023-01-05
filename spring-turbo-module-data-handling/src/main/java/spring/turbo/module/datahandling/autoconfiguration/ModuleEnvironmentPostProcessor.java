0cd8ae90f5aaf7d9b098dedabac1deec18f56f80
应卓 <yingzhor@gmail.com>
2023-01-05 11:15:19 +0800

    2023-01-05 11:15:18

--- spring-turbo-module-data-handling/src/main/java/spring/turbo/module/datahandling/autoconfiguration/ModuleEnvironmentPostProcessor.java
+++ spring-turbo-module-data-handling/src/main/java/spring/turbo/module/datahandling/autoconfiguration/ModuleEnvironmentPostProcessor.java
@@ -8,110 +8,21 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package spring.turbo.module.datahandling.autoconfiguration;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.core.Ordered;
-import org.springframework.core.env.ConfigurableEnvironment;
-import spring.turbo.core.AbstractResourceBasedEnvironmentPostProcessor;
-import spring.turbo.io.ResourceOption;
-import spring.turbo.io.ResourceOptions;
-import spring.turbo.util.StringFormatter;
-
-import java.util.ArrayList;
-
-import static spring.turbo.core.Dependencies.*;
+import spring.turbo.core.env.AbstractModuleEnvironmentPostProcessor;
 
 /**
  * @author 应卓
  * @since 2.0.7
  */
-public class ModuleEnvironmentPostProcessor extends AbstractResourceBasedEnvironmentPostProcessor {
+public class ModuleEnvironmentPostProcessor extends AbstractModuleEnvironmentPostProcessor {
 
     /**
      * 默认构造方法
      */
     public ModuleEnvironmentPostProcessor() {
-        super(Ordered.LOWEST_PRECEDENCE - 200);
-    }
+        super(Ordered.LOWEST_PRECEDENCE - 200, "spring-turbo-data-handling", "spring-turbo-module-data-handling");
 
-    @Override
-    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        final ResourceOption resourceOption = loadResource(application);
-
-        if (resourceOption.isAbsent()) {
-            return;
-        }
-
-        final var propertySource = toPropertySource(resourceOption);
-        if (propertySource != null) {
-            final var propertySources = environment.getPropertySources();
-            propertySources.addLast(propertySource);
-        }
-    }
-
-    private ResourceOption loadResource(SpringApplication application) {
-        final var resourceLocations = new ArrayList<String>();
-
-        // ---
-        for (var appDir : getApplicationDirectories(application)) {
-            resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-data-handling.properties", appDir));
-            resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-data-handling.xml", appDir));
-            if (YAML_PRESENT) {
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-data-handling.yaml", appDir));
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-data-handling.yml", appDir));
-            }
-            if (HOCON_PRESENT) {
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-data-handling.conf", appDir));
-            }
-            if (TOML_PRESENT) {
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-data-handling.toml", appDir));
-            }
-        }
-
-        // ---
-        resourceLocations.add("classpath:spring-turbo-module-data-handling.properties");
-        resourceLocations.add("classpath:spring-turbo-module-data-handling.xml");
-        if (YAML_PRESENT) {
-            resourceLocations.add("classpath:spring-turbo-module-data-handling.yaml");
-            resourceLocations.add("classpath:spring-turbo-module-data-handling.yml");
-        }
-        if (HOCON_PRESENT) {
-            resourceLocations.add("classpath:spring-turbo-module-data-handling.conf");
-        }
-        if (TOML_PRESENT) {
-            resourceLocations.add("classpath:spring-turbo-module-data-handling.toml");
-        }
-
-        // ---
-        resourceLocations.add("classpath:META-INF/spring-turbo-module-data-handling.properties");
-        resourceLocations.add("classpath:META-INF/spring-turbo-module-data-handling.xml");
-        if (YAML_PRESENT) {
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-data-handling.yaml");
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-data-handling.yml");
-        }
-        if (HOCON_PRESENT) {
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-data-handling.conf");
-        }
-        if (TOML_PRESENT) {
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-data-handling.toml");
-        }
-
-        // ---
-        resourceLocations.add("classpath:conf/spring-turbo-module-data-handling.properties");
-        resourceLocations.add("classpath:conf/spring-turbo-module-data-handling.xml");
-        if (YAML_PRESENT) {
-            resourceLocations.add("classpath:conf/spring-turbo-module-data-handling.yaml");
-            resourceLocations.add("classpath:conf/spring-turbo-module-data-handling.yml");
-        }
-        if (HOCON_PRESENT) {
-            resourceLocations.add("classpath:conf/spring-turbo-module-data-handling.conf");
-        }
-        if (TOML_PRESENT) {
-            resourceLocations.add("classpath:conf/spring-turbo-module-data-handling.toml");
-        }
-
-        return ResourceOptions.builder()
-                .add(resourceLocations)
-                .build();
     }
 
 }
--- spring-turbo-module-webmvc/src/main/java/spring/turbo/module/webmvc/autoconfiguration/ModuleEnvironmentPostProcessor.java
+++ spring-turbo-module-webmvc/src/main/java/spring/turbo/module/webmvc/autoconfiguration/ModuleEnvironmentPostProcessor.java
@@ -8,110 +8,20 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package spring.turbo.module.webmvc.autoconfiguration;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.core.Ordered;
-import org.springframework.core.env.ConfigurableEnvironment;
-import spring.turbo.core.AbstractResourceBasedEnvironmentPostProcessor;
-import spring.turbo.io.ResourceOption;
-import spring.turbo.io.ResourceOptions;
-import spring.turbo.util.StringFormatter;
-
-import java.util.ArrayList;
-
-import static spring.turbo.core.Dependencies.*;
+import spring.turbo.core.env.AbstractModuleEnvironmentPostProcessor;
 
 /**
  * @author 应卓
  * @since 2.0.7
  */
-public class ModuleEnvironmentPostProcessor extends AbstractResourceBasedEnvironmentPostProcessor {
+public class ModuleEnvironmentPostProcessor extends AbstractModuleEnvironmentPostProcessor {
 
     /**
      * 默认构造方法
      */
     public ModuleEnvironmentPostProcessor() {
-        super(Ordered.LOWEST_PRECEDENCE - 200);
-    }
-
-    @Override
-    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        final ResourceOption resourceOption = loadResource(application);
-
-        if (resourceOption.isAbsent()) {
-            return;
-        }
-
-        final var propertySource = toPropertySource(resourceOption);
-        if (propertySource != null) {
-            final var propertySources = environment.getPropertySources();
-            propertySources.addLast(propertySource);
-        }
-    }
-
-    private ResourceOption loadResource(SpringApplication application) {
-        final var resourceLocations = new ArrayList<String>();
-
-        // ---
-        for (var appDir : getApplicationDirectories(application)) {
-            resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-webmvc.properties", appDir));
-            resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-webmvc.xml", appDir));
-            if (YAML_PRESENT) {
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-webmvc.yaml", appDir));
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-webmvc.yml", appDir));
-            }
-            if (HOCON_PRESENT) {
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-webmvc.conf", appDir));
-            }
-            if (TOML_PRESENT) {
-                resourceLocations.add(StringFormatter.format("file:{}/spring-turbo-module-webmvc.toml", appDir));
-            }
-        }
-
-        // ---
-        resourceLocations.add("classpath:spring-turbo-module-webmvc.properties");
-        resourceLocations.add("classpath:spring-turbo-module-webmvc.xml");
-        if (YAML_PRESENT) {
-            resourceLocations.add("classpath:spring-turbo-module-webmvc.yaml");
-            resourceLocations.add("classpath:spring-turbo-module-webmvc.yml");
-        }
-        if (HOCON_PRESENT) {
-            resourceLocations.add("classpath:spring-turbo-module-webmvc.conf");
-        }
-        if (TOML_PRESENT) {
-            resourceLocations.add("classpath:spring-turbo-module-webmvc.toml");
-        }
-
-        // ---
-        resourceLocations.add("classpath:META-INF/spring-turbo-module-webmvc.properties");
-        resourceLocations.add("classpath:META-INF/spring-turbo-module-webmvc.xml");
-        if (YAML_PRESENT) {
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-webmvc.yaml");
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-webmvc.yml");
-        }
-        if (HOCON_PRESENT) {
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-webmvc.conf");
-        }
-        if (TOML_PRESENT) {
-            resourceLocations.add("classpath:META-INF/spring-turbo-module-webmvc.toml");
-        }
-
-        // ---
-        resourceLocations.add("classpath:conf/spring-turbo-module-webmvc.properties");
-        resourceLocations.add("classpath:conf/spring-turbo-module-webmvc.xml");
-        if (YAML_PRESENT) {
-            resourceLocations.add("classpath:conf/spring-turbo-module-webmvc.yaml");
-            resourceLocations.add("classpath:conf/spring-turbo-module-webmvc.yml");
-        }
-        if (HOCON_PRESENT) {
-            resourceLocations.add("classpath:conf/spring-turbo-module-webmvc.conf");
-        }
-        if (TOML_PRESENT) {
-            resourceLocations.add("classpath:conf/spring-turbo-module-webmvc.toml");
-        }
-
-        return ResourceOptions.builder()
-                .add(resourceLocations)
-                .build();
+        super(Ordered.LOWEST_PRECEDENCE - 200, "spring-turbo-webmvc", "spring-turbo-module-webmvc");
     }
 
 }
