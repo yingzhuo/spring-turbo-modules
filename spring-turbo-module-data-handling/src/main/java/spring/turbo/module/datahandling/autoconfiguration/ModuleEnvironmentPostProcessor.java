fe0e735a237faa037014c89e5e269fe2f860d457
应卓 <yingzhor@gmail.com>
2023-01-06 10:45:12 +0800

    2023-01-06 10:45:11

--- spring-turbo-module-data-handling/src/main/java/spring/turbo/module/datahandling/autoconfiguration/ModuleEnvironmentPostProcessor.java
+++ spring-turbo-module-data-handling/src/main/java/spring/turbo/module/datahandling/autoconfiguration/ModuleEnvironmentPostProcessor.java
@@ -9,20 +9,30 @@
 package spring.turbo.module.datahandling.autoconfiguration;
 
 import org.springframework.core.Ordered;
-import spring.turbo.core.env.AbstractModuleEnvironmentPostProcessor;
+import org.springframework.core.env.Environment;
+import spring.turbo.core.ApplicationHomeDir;
+import spring.turbo.core.env.AbstractConventionBasedEnvironmentPostProcessor;
+
+import java.util.List;
 
 /**
  * @author 应卓
  * @since 2.0.7
  */
-public class ModuleEnvironmentPostProcessor extends AbstractModuleEnvironmentPostProcessor {
+public class ModuleEnvironmentPostProcessor extends AbstractConventionBasedEnvironmentPostProcessor {
 
     /**
      * 默认构造方法
      */
     public ModuleEnvironmentPostProcessor() {
-        super(Ordered.LOWEST_PRECEDENCE - 200, "spring-turbo-data-handling", "spring-turbo-module-data-handling");
+        super.setOrder(Ordered.LOWEST_PRECEDENCE - 200);
+    }
 
+    @Override
+    public List<ResourceOptionGroup> getResourceOptionGroups(Environment environment, ApplicationHomeDir homeDir) {
+        return List.of(
+                generateConventionBasedGroup(homeDir, "<spring-turbo-module-data-handling>", "spring-turbo-data-handling")
+        );
     }
 
 }
