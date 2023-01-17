4eb9f50156a2d4698401495e323c2de8060e87b5
应卓 <yingzhor@gmail.com>
2023-01-17 19:09:20 +0800

    2023-01-17 19:09:19

---
+++ spring-turbo-module-webmvc/src/main/java/spring/turbo/module/webmvc/requestcondition/Versioned.java
@@ -0,0 +1,27 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.webmvc.requestcondition;
+
+import java.lang.annotation.*;
+
+/**
+ * @author 应卓
+ * @since 2.0.9
+ */
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Versioned {
+
+    public String value();
+
+    public boolean ignoreCase() default false;
+
+}
---
+++ spring-turbo-module-webmvc/src/main/java/spring/turbo/module/webmvc/version/VersionResolver.java
@@ -0,0 +1,25 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.webmvc.version;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+/**
+ * @author 应卓
+ * @since 2.0.9
+ */
+@FunctionalInterface
+public interface VersionResolver {
+
+    @Nullable
+    public String resolve(@NonNull HttpServletRequest request);
+
+}
