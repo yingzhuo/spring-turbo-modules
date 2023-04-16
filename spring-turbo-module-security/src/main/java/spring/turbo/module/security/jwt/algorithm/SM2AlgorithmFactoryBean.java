840a3c55cea432c9ce53b3cdf5ea90c56834606c
应卓 <yingzhor@gmail.com>
2023-04-17 04:38:11 +0800

    2023-04-17 04:38:10

--- spring-turbo-module-security/src/main/java/spring/turbo/module/security/jwt/algorithm/SM2AlgorithmFactoryBean.java
+++ spring-turbo-module-security/src/main/java/spring/turbo/module/security/jwt/algorithm/SM2AlgorithmFactoryBean.java
@@ -13,6 +13,8 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.SignatureGenerationException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.lang.Nullable;
 import spring.turbo.util.Asserts;
@@ -24,6 +26,8 @@ import spring.turbo.util.crypto.Base64;
  * @author 应卓
  * @since 2.2.2
  */
+@Getter
+@Setter
 public class SM2AlgorithmFactoryBean implements FactoryBean<Algorithm> {
 
     private String privateKey;
@@ -49,44 +53,19 @@ public class SM2AlgorithmFactoryBean implements FactoryBean<Algorithm> {
         return Algorithm.class;
     }
 
-    public String getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(String privateKey) {
-        this.privateKey = privateKey;
-    }
-
-    public String getPublicKey() {
-        return publicKey;
-    }
-
-    public void setPublicKey(String publicKey) {
-        this.publicKey = publicKey;
-    }
-
-    @Nullable
-    public String getWithId() {
-        return withId;
-    }
-
-    public void setWithId(String withId) {
-        this.withId = withId;
-    }
-
     // -----------------------------------------------------------------------------------------------------------------
 
     private static class SM2Algorithm extends AbstractAlgorithm {
 
         private final SM2 sm2;
-        private String withId = "1234567812345678";
+
+        @Nullable
+        private String withId = null;
 
         public SM2Algorithm(String publicKey, String privateKey, @Nullable String withId) {
             super("SM2", "SM2国密算法");
             this.sm2 = new SM2(privateKey, publicKey);
-            if (withId != null) {
-                this.withId = withId;
-            }
+            this.withId = withId;
         }
 
         @Override
@@ -94,7 +73,7 @@ public class SM2AlgorithmFactoryBean implements FactoryBean<Algorithm> {
             try {
                 final byte[] signatureBytes = Base64.toBytes(jwt.getSignature());
                 final byte[] dataBytes = super.combineHeaderAndPayload(jwt);
-                boolean success = sm2.verify(dataBytes, signatureBytes, withId.getBytes());
+                boolean success = sm2.verify(dataBytes, signatureBytes, getWithIdBytes());
                 if (!success) {
                     throw new SignatureVerificationException(this);
                 }
@@ -106,9 +85,18 @@ public class SM2AlgorithmFactoryBean implements FactoryBean<Algorithm> {
         @Override
         public byte[] sign(byte[] contentBytes) throws SignatureGenerationException {
             try {
-                return sm2.sign(contentBytes, withId.getBytes());
+                return sm2.sign(contentBytes, getWithIdBytes());
             } catch (Throwable e) {
                 throw new SignatureGenerationException(this, e);
+            }
+        }
+
+        @Nullable
+        private byte[] getWithIdBytes() {
+            if (this.withId != null) {
+                return this.withId.getBytes();
+            } else {
+                return null;
             }
         }
     }
