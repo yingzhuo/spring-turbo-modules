66e7d49ab2e50e2d6ce97abf43ec648b5ac2012d
应卓 <yingzhor@gmail.com>
2024-06-12 16:18:06 +0800

    2024-06-12 16:18:02

---
+++ spring-turbo-module-jackson/src/main/java/spring/turbo/module/jackson/module/SpringTurboJackson2Module.java
@@ -0,0 +1,38 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.jackson.module;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.core.util.VersionUtil;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import spring.turbo.SpringTurboVersion;
+
+/**
+ * @author 应卓
+ *
+ * @since 3.3.1
+ */
+public class SpringTurboJackson2Module extends SimpleModule {
+
+    private static final Version VERSION = VersionUtil.parseVersion(SpringTurboVersion.CURRENT, "com.github.yingzhuo",
+            "spring-turbo");
+
+    /**
+     * 默认构造方法
+     */
+    public SpringTurboJackson2Module() {
+        super(SpringTurboJackson2Module.class.getName(), VERSION);
+    }
+
+    @Override
+    public void setupModule(SetupContext context) {
+        super.setupModule(context);
+    }
+
+}
