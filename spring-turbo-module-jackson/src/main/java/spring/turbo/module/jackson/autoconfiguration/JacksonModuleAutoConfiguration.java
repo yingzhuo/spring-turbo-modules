42f7385d448e49d69749f5fe7c303c25099e5f3c
应卓 <yingzhor@gmail.com>
2024-06-12 08:16:42 +0800

    2024-06-12 08:16:38

---
+++ spring-turbo-module-jackson/src/main/java/spring/turbo/module/jackson/autoconfiguration/JacksonModuleAutoConfiguration.java
@@ -0,0 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.jackson.autoconfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.lang.Nullable;
+import spring.turbo.module.jackson.util.JacksonModuleUtils;
+
+import java.util.Objects;
+
+/**
+ * @author 应卓
+ *
+ * @since 3.3.1
+ */
+@AutoConfiguration
+public class JacksonModuleAutoConfiguration {
+
+    @Autowired(required = false)
+    private void initModules(@Nullable ObjectMapper objectMapper) {
+        if (objectMapper != null) {
+            JacksonModuleUtils.loadAndRegisterModules(objectMapper, Objects::nonNull);
+        }
+    }
+
+}
