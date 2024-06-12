9174555ade3c9decbcf5b816a4413841715fa27e
应卓 <yingzhor@gmail.com>
2024-06-12 14:07:03 +0800

    2024-06-12 14:06:59

---
+++ spring-turbo-module-jackson/src/main/java/spring/turbo/module/jackson/autoconfiguration/JsonPathAutoConfiguration.java
@@ -0,0 +1,34 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.jackson.autoconfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author 应卓
+ *
+ * @since 3.3.1
+ */
+@AutoConfiguration
+@ConditionalOnClass(name = "com.fasterxml.jackson.databind.ObjectMapper")
+public class JsonPathAutoConfiguration {
+
+    @Bean
+    public Configuration jsonPathConfiguration(final ObjectMapper mapper) {
+        return Configuration.builder().jsonProvider(new JacksonJsonProvider(mapper))
+                .mappingProvider(new JacksonMappingProvider(mapper)).build();
+    }
+
+}
---
+++ spring-turbo-module-jackson/src/main/java/spring/turbo/module/jackson/util/JsonUtils.java
@@ -0,0 +1,113 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.jackson.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.TypeRef;
+import spring.turbo.io.IOExceptionUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.lang.reflect.Type;
+
+import static spring.turbo.core.SpringUtils.getRequiredBean;
+
+/**
+ * @author 应卓
+ *
+ * @since 3.3.1
+ */
+public final class JsonUtils {
+
+    /**
+     * 私有构造方法
+     */
+    private JsonUtils() {
+        super();
+    }
+
+    public static String toJson(Object obj) {
+        try {
+            return getRequiredBean(ObjectMapper.class).writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw IOExceptionUtils.toUnchecked(e);
+        }
+    }
+
+    public static <T> T parseJson(String json, Class<T> objClass) {
+        try {
+            return getRequiredBean(ObjectMapper.class).readValue(json, objClass);
+        } catch (JsonProcessingException e) {
+            throw IOExceptionUtils.toUnchecked(e);
+        }
+    }
+
+    public static <T> T parseJson(String json, String jsonPath, final Class<T> objClass) {
+        return parseJson(json, jsonPath, new TypeRef<T>() {
+            @Override
+            public Type getType() {
+                return objClass;
+            }
+        });
+    }
+
+    public static <T> T parseJson(String json, String jsonPath, TypeRef<T> typeRef) {
+        return JsonPath.using(getRequiredBean(Configuration.class)).parse(json).read(jsonPath, typeRef);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public static <T> T parseJson(InputStream json, Class<T> objClass) {
+        try {
+            return getRequiredBean(ObjectMapper.class).readValue(json, objClass);
+        } catch (IOException e) {
+            throw IOExceptionUtils.toUnchecked(e);
+        }
+    }
+
+    public static <T> T parseJson(InputStream json, String jsonPath, final Class<T> objClass) {
+        return parseJson(json, jsonPath, new TypeRef<T>() {
+            @Override
+            public Type getType() {
+                return objClass;
+            }
+        });
+    }
+
+    public static <T> T parseJson(InputStream json, String jsonPath, TypeRef<T> typeRef) {
+        return JsonPath.using(getRequiredBean(Configuration.class)).parse(json).read(jsonPath, typeRef);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    public static <T> T parseJson(Reader json, Class<T> objClass) {
+        try {
+            return getRequiredBean(ObjectMapper.class).readValue(json, objClass);
+        } catch (IOException e) {
+            throw IOExceptionUtils.toUnchecked(e);
+        }
+    }
+
+    public static <T> T parseJson(Reader json, String jsonPath, final Class<T> objClass) {
+        return parseJson(json, jsonPath, new TypeRef<T>() {
+            @Override
+            public Type getType() {
+                return objClass;
+            }
+        });
+    }
+
+    public static <T> T parseJson(Reader json, String jsonPath, TypeRef<T> typeRef) {
+        return JsonPath.using(getRequiredBean(Configuration.class)).parse(json).read(jsonPath, typeRef);
+    }
+}
