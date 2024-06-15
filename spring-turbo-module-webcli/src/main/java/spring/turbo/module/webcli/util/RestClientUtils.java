0d91fb6d04b55f03eb4cc01d4cce01cdf738a2ec
应卓 <yingzhor@gmail.com>
2024-06-15 08:51:19 +0800

    2024-06-15 08:51:15

---
+++ spring-turbo-module-webcli/src/main/java/spring/turbo/module/webcli/util/RestClientUtils.java
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.webcli.util;
+
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 应卓
+ *
+ * @since 3.3.1
+ */
+public final class RestClientUtils {
+
+    /**
+     * 私有构造方法
+     */
+    private RestClientUtils() {
+        super();
+    }
+
+    public static RestClient fromRestTemplate(RestTemplate restTemplate) {
+        return RestClient.create(restTemplate);
+    }
+
+}
