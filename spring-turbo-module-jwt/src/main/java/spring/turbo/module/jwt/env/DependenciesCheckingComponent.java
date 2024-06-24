5d2152af37b57bdd188575e7811c4a95df67bda3
应卓 <yingzhor@gmail.com>
2024-06-24 10:09:32 +0800

    2024-06-24 10:09:30

---
+++ spring-turbo-module-jwt/src/main/java/spring/turbo/module/jwt/env/DependenciesCheckingComponent.java
@@ -0,0 +1,58 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.jwt.env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.diagnostics.FailureAnalysis;
+import org.springframework.boot.diagnostics.FailureAnalyzer;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.lang.Nullable;
+import spring.turbo.util.ClassUtils;
+
+/**
+ * @author 应卓
+ * @since 3.1.1
+ */
+class DependenciesCheckingComponent implements EnvironmentPostProcessor, FailureAnalyzer {
+
+    private static final boolean PRESENT_HUTOOL = ClassUtils.isPresent("cn.hutool.jwt.signers.JWTSigner");
+    private static final boolean PRESENT_JAVA_JWT = ClassUtils.isPresent("com.auth0.jwt.algorithms.Algorithm");
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        if (!(PRESENT_HUTOOL || PRESENT_JAVA_JWT)) {
+            throw new BadDependencies();
+        }
+    }
+
+    @Nullable
+    @Override
+    public FailureAnalysis analyze(Throwable failure) {
+        if (failure instanceof BadDependencies) {
+            var desc = """
+                    Make sure at least one of library added to your classpath.
+                    1. "com.auth0:java-jwt:<version>"
+                    2. "cn.hutool:hutool-jwt:<version>"
+                    """;
+
+            return new FailureAnalysis(
+                    desc,
+                    "Re-check your Dependencies",
+                    failure
+            );
+        }
+
+        return null;
+    }
+
+    private static class BadDependencies extends RuntimeException {
+    }
+
+}
