a56433ea598bf77f9f87dc0aabe0afcfe79078c7
应卓 <yingzhor@gmail.com>
2024-06-30 16:11:49 +0800

    2024-06-30 16:11:46

---
+++ spring-turbo-module-webmvc/src/main/java/spring/turbo/module/webmvc/autoconfiguration/DataBinderInitializingAdviceAutoConfiguration.java
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.webmvc.autoconfiguration;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import spring.turbo.module.webmvc.databinding.DataBinderInitializingAdvice;
+
+/**
+ * @author 应卓
+ * @since 3.3.2
+ */
+@AutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class DataBinderInitializingAdviceAutoConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public DataBinderInitializingAdvice dataBinderInitializingAdvice() {
+        return new DataBinderInitializingAdvice();
+    }
+
+}
---
+++ spring-turbo-module-webmvc/src/main/java/spring/turbo/module/webmvc/databinding/DataBinderInitializingAdvice.java
@@ -0,0 +1,28 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.module.webmvc.databinding;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+import spring.turbo.databinding.SmartBindingErrorProcessor;
+
+/**
+ * @author 应卓
+ * @since 3.3.2
+ */
+@ControllerAdvice
+public class DataBinderInitializingAdvice {
+
+    @InitBinder
+    public void init(WebDataBinder dataBinder) {
+        dataBinder.setBindingErrorProcessor(SmartBindingErrorProcessor.getInstance());
+    }
+
+}
